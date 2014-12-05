c5809ef0c433ec6b0d885e816673dff93f2525c0
Oskar Dillén <dillenmeister@gmail.com>
2014-12-05 10:53:34 +0100

    Fixade så den funkar OK

--- src/scripts/content/lernia-helpdesk.js
+++ src/scripts/content/lernia-helpdesk.js
@@ -1,18 +1,18 @@
-/*jslint indent: 2, unparam: true*/
+﻿/*jslint indent: 2, unparam: true*/
 /*global $: false, document: false, togglbutton: false*/
 'use strict';
 
 togglbutton.render('#title-wrapper:not(.toggl)', { observe: true }, function (elem) {
     
-    var link, description,
-      titleElem = $('#title-wrapper h1', elem);    
-
-    description = titleElem.innerText;
-
-    link = togglbutton.createTimerLink({
+    var link = togglbutton.createTimerLink({
         className: 'lernia-helpdesk',
-        description: description        
+        description: document.title,
+        projectName: 'Lernia Applikationsförvaltning'
     });
-
-    $('#title-wrapper h1').appendChild(link);
+    
+    if ($('h1 span', elem)) {
+        $('h1 span').appendChild(link);
+    } else {
+        $('h1', elem).appendChild(link);        
+    }
 });
